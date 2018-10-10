3dd973ec50f46486b29b70aba229f7577833662a
jordanbenzacken <jbzkjbzk@gmail.com>
2018-10-11 00:48:42 +0200

    POC working

--- event.js
+++ event.js
@@ -1,16 +1,106 @@
+const Moment = require('moment')
+const MomentRange = require('moment-range');
+const moment = MomentRange.extendMoment(Moment);
+
 const eventList = []
 
 const Event = function(opening, recurring, startDate, endDate) {
     this.opening = opening
     this.recurring = recurring
     this.startDate = startDate
-    this.endDate = endDate
+
+    //Normalize, if no endDate, finish in one hour by convention
+    this.endDate = endDate ? endDate : moment(startDate).add(1, 'hour')
 
     eventList.push(this)
 }
 
-Event.prototype.availabilities = (fromDate, toDate) => {
-    return //Something awesome
+//Generate a list with no recurring slots
+const getSimpleSlots = (event, fromDate, toDate) => {
+    const numberOfWeeks = moment(toDate).diff(moment(fromDate), 'week') + 1
+    if (event.startDate > toDate) {
+        //No need to go deeper
+        return null
+    }
+    if (!event.recurring) {
+        return event
+    }
+
+    //if recurring, generate simple slots
+    const offSet = moment(fromDate).diff(moment(event.startDate), 'week') + 1
+    const events = Array.from(new Array(numberOfWeeks), (val, index) => ({
+        opening: event.opening,
+        recurring: false,
+        startDate: dateWeeksAdded(event.startDate, index + offSet),
+        endDate: dateWeeksAdded(event.endDate, index + offSet)
+    }))
+    return events
+}
+
+//map reduce to have array of simple events
+const getSimpleEventList = (fromDate, toDate) => {
+    return eventList.map((event) => getSimpleSlots(event, fromDate, toDate)).reduce(flattenArrays, [])
+}
+
+const flattenArrays = (acc, curr) => {
+    let res = acc
+    if (curr.length) {
+        res = acc.concat(curr)
+    } else if (curr) {
+        res.push(curr)
+    }
+    return res
+}
+
+const dateWeeksAdded = (date, numberOfWeeks) => {
+    return moment(date).add(numberOfWeeks, 'week')
+}
+
+//Filter to get only slots included in this period
+const getIncludedSlots = (eventList, period) => {
+    return eventList.filter((event) => {
+        if (period.contains(event.startDate) || period.contains(event.endDate)) {
+            return true
+        }
+        return false
+    })
+}
+
+const generateAnswer = (rangedSlots) => {
+    const orderedAvailableSlot = rangedSlots.filter(slot => slot.opening).sort(o => o.moment(slot.startDate).unix())
+    const orderedUnAvailableSlot = rangedSlots.filter(slot => !slot.opening).sort(o => o.moment(slot.startDate).unix())
+    const orderedAvailableNormalizedSlot = orderedAvailableSlot.map(slot => {
+        return Array.from(moment.range(slot.startDate, slot.endDate).byRange(moment.range(moment(), moment().add(30, 'minute')), { excludeEnd: true })).map(x => {
+            return moment.range(x, moment(x).add(30, 'minute'))
+        }
+        )
+    }
+    ).reduce(flattenArrays, [])
+    const orderedUnAvailableNormalizedSlot = orderedUnAvailableSlot.map(slot => {
+        return Array.from(moment.range(slot.startDate, slot.endDate).byRange(moment.range(moment(), moment().add(30, 'minute')), { excludeEnd: true })).map(x => {
+            return moment.range(x, moment(x).add(30, 'minute'))
+        }
+        )
+    }
+    ).reduce(flattenArrays, [])
+    const slotsMatch = orderedAvailableNormalizedSlot.filter(av => {
+        let shouldStay = true
+        orderedUnAvailableNormalizedSlot.forEach(unav => {
+            if (unav.intersect(av)) {
+                debugger
+                shouldStay = false
+            }
+        })
+        return shouldStay
+    })
+    return slotsMatch
+}
+
+Event.prototype.getAvailabilities = (fromDate, toDate) => {
+    const simpleEventList = getSimpleEventList(fromDate, toDate)
+    const period = moment.range(fromDate, toDate)
+    const rangedSlots = getIncludedSlots(simpleEventList, period)
+    return generateAnswer(rangedSlots)
 }
 
 module.exports = Event
--- main.js
+++ main.js
@@ -6,13 +6,13 @@ let endDate = new Date(2018, 6, 1, 14, 00) // July 1st, 14:00
 new Event(true, true, startDate, endDate) // weekly recurring opening in calendar
 
 startDate = new Date(2018, 6, 8, 11, 30) // July 8th 11:30
-endDate = new Date() // July 8th 11:30
+endDate = new Date(2018, 6, 8, 12, 30) // July 8th 11:30
 new Event(false, false, startDate, endDate) // intervention scheduled
 
-const fromDate = new Date(2018, 7, 4, 10, 00)
-const toDate = new Date(2018, 7, 10, 10, 00)
+const fromDate = new Date(2018, 6, 4, 10, 00)
+const toDate = new Date(2018, 6, 10, 10, 00)
 
-Event.prototype.availabilities(fromDate, toDate)
+console.log(Event.prototype.getAvailabilities(fromDate, toDate))
 
 /*
  * Answer should be : 
